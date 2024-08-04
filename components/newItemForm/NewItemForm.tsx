b94ae3fb9e88f2cfb9e91e46765ab2d37401e19c
Tanja Kobler <kobler.tanja@gmail.com>
2024-08-04 22:06:44 +0200

    bugfix

--- components/newItemForm/NewItemForm.tsx
+++ components/newItemForm/NewItemForm.tsx
@@ -196,7 +196,7 @@ const NewItemForm: React.FC = () => {
                 </select>
             </div>
 
-            /*google api file upload*/
+            {/*google api file upload*/}
 
             <div className="form-group">
                 <label htmlFor="file-upload">Choose Item Image:</label>
