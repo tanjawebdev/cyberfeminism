cead71ef171ce50368f8c9f622fd6524274e8470
Tanja Kobler <kobler.tanja@gmail.com>
2025-05-10 14:46:59 +0200

    bug fix mobil

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,7 +7,6 @@ import "@styles/globalsDesktop.scss";
 export const metadata: Metadata = {
   title: "Cyberfeminism",
   description: "A digital intervention",
-  viewport: "width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no",
   icons: {
     icon: [
       { rel: "icon", url: "/favicon.ico" },
